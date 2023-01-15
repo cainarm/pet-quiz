1432261efc1d6432e942199c872cda6e586fb053
cainarm <cainarm@yahoo.com.br>
2023-01-15 02:58:22 -0300

    build: initial structure

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Pet Quiz</title>
+        <meta name="description" content="Pet Quiz" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </div>
+  );
+}
